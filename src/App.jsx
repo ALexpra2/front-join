c2ed7428ad89ef17dd1b09345793bfa6d51198ee
Alexpra2 <alexpra2@gmail.com>
2025-04-02 01:49:09 +0200

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Home from './Home.jsx'
+import ItemDetailPage from './ItemDetailPage.jsx'
+
 const App = () => {
+  const [data, setData] = useState(null)
+  const urlApi = 'http://localhost:3000'
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(urlApi)
+      const resData = await response.json()
+      setData(resData)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // cargo fetch data la primera vez que se carga el componente
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
-  <></>      
+    <Router>
+      <div>
+        <nav>
+          <Link to= "/">Inicio</Link>
+        </nav>
+        {/*como data lo traigo asincrono porque lo introduzco setData(resData)=>data, tengo k hacer el cargando por si no han venido los datos todavia que no se rompa*/}
+        {data === null
+          ? ( <div>Cargando...</div>) 
+          :
+          (
+            <Routes>
+              <Route path="/" element={<Home data= {data} />} />
+              {/*genero las rutas dinamicas con el id de cada una y cargo esas propiedades en el componente ItemDetailPage*/}
+            {data.map(item => (
+              <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item} />} />
+            ))}
+            </Routes>
+          )
+        }
+      </div>
+    </Router>
   )
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,19 @@
-const Home = () => {
+import { Link } from 'react-router-dom'
+
+//Genero la home con las propiedades que me he traido desde app.jsx 
+const Home = ({data}) => {
   return (
     <>
-    
+      <h2>Lista de datos</h2>
+      <ul>
+        {/*Recorro el array de objetos que me he traido y muestro el id y el title recorriendo data*/}
+        {/*El id lo uso como key para que no me de error en la consola*/}
+        {data.map(item => (
+          <li key={item._id}>
+            <Link to={`/${item._id}`}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 };
